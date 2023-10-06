f9a0bddfef2beda458b7c2c3de03a39e048f40e1
CodeSandbox <support@codesandbox.io>
2023-10-06 14:10:43 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas
+        dolore nostrum exercitationem labore, nesciunt ea cumque quisquam
+        corporis libero ullam sit dolores iste laboriosam voluptatum doloremque,
+        voluptate corrupti adipisci.
+      </p>
+    </>
+  );
+}
